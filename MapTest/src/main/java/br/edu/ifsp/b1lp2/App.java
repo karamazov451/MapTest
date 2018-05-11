86f5e6ce3ec9d64303d59fa84e7850bdb7b717ae
karamazov451 <30283021+karamazov451@users.noreply.github.com>
2018-05-11 17:48:16 -0300

    Update App.java

--- MapTest/src/main/java/br/edu/ifsp/b1lp2/App.java
+++ MapTest/src/main/java/br/edu/ifsp/b1lp2/App.java
@@ -11,7 +11,7 @@ public class App
 		int j;
 		int ocorrencias;
 		HashMap<String, Integer> hmap = new HashMap<String, Integer>();
-		String frase = "Kirby does his taxes Kirby";
+		String frase = "Kirby does his taxes";
 		String palavras[] = frase.split(" ");
 		
 		for(i = 0; i < palavras.length; i++)
